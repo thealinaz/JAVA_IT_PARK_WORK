8431f8b843595473cf256067020f779cc0821a81
IT_PARK <it@park.com>
2017-03-11 19:06:57 +0300

    add classworks

--- ClassWorks/Task2/Program.java
+++ ClassWorks/Task2/Program.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
 class Program {
-
-public static int sum (int a, int b) {
-        int s = 0;
-     for (int i = a; i <= b; i++) {
-          s += i;
-       }
-      return s;
+  public static int sum (int a, int b) {int s = 0;
+    for (int i = a; i <= b; i++)
+      s += i;
+    return s;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter x, y: ");
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        int c = sum (x, y);
+    System.out.println("Enter x, y: ");
+    
+    int x = scanner.nextInt();
+    int y = scanner.nextInt();
+    int c = sum (x, y);
 
-        System.out.println("Sum of sequence: " + c);
+    System.out.println("Sum of" + c);
     }
-}
+} 

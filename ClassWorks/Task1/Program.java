696626dca082c2750206216d2ac43f0864dacade
IT_PARK <it@park.com>
2017-03-11 17:30:14 +0300

    add classworks

--- ClassWorks/Task1/Program.java
+++ ClassWorks/Task1/Program.java
@@ -3,34 +3,27 @@ import java.util.Scanner;
 class Program {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter x: ");
+        int x = scanner.nextInt();
+        int m = 0;
+        boolean flag = false;
+
         System.out.println("Enter size of array: ");
         int n = scanner.nextInt();
 
         int a [] = new int[n];
-        int y = 0, x = 0;
         System.out.println("Enter elements of array: ");
 
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
-        }
-
-        int min = a[0], max = a[0];
-
-        for (int i = 0; i < n; i++) {
-            if (min > a[i]) {
-            	min = a[i];
-            	y=i;
-            }
-            if (max < a[i]) {
-            	max = a[i];
-            	x=i;
+            if (a[i] == x) {
+                m=x;
+                flag = true;
             }
         }
-        a[y] = max;
-        a[x] = min;
 		
-		System.out.println("New array: ");
-        for (int i = 0; i < n; i++) 
-            System.out.print(a[i] + " ");
+		if (flag) System.out.println('Position: ' + m);
+        else System.out.println(-1);
 	}
 }

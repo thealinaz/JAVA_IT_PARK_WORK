5d74ad0774dd72347878f3517988a9165dc3c438
IT_PARK <it@park.com>
2017-03-11 19:51:34 +0300

    add classworks

--- ClassWorks/Task4/Program.java
+++ ClassWorks/Task4/Program.java
@@ -4,17 +4,18 @@ class Program{
 	public static void main(String[] args) {
 		Random random = new Random();
 		int ages[] = new int[100];
-		int a[] = new int[120];
+		int a[] = new int[121];
 
-		for(int i = 0; i < ages.length; i++) {
+		for(int i = 0; i < ages.length; i++)
 			ages[i] = random.nextInt(120);
+		
+		for(int i = 0; i < 100; i++) 
 			a[ages[i]] ++;
-		}
 
 		for(int i = 0; i < 100; i++)
 			System.out.println(ages[i]);
 
-		for(int i = 0; i < 100; i++) 
+		for(int i = 0; i <= 120; i++) 
 			System.out.println("Age " + i + " =" + a[i]);
 	}
 }

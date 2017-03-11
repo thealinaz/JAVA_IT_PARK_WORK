b24909f1dfabfcc224286f46087ac91a6665e445
IT_PARK <it@park.com>
2017-03-11 18:04:04 +0300

    add classworks

---
+++ ClassWorks/Task2/Program.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Program {
+
+public static int sum (int a, int b) {
+        int s = 0;
+     for (int i = a; i <= b; i++) {
+          s += i;
+       }
+      return s;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter x, y: ");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int c = sum (x, y);
+
+        System.out.println(c);
+    }
+}

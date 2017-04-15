12a9f2e90ab02d7e31e6b5c7dabc57c3d7749071
Student7 <thealinaz@mail.ru>
2017-04-15 17:39:17 +0300

    add

--- ClassWorks/Task10/src/Main.java
+++ ClassWorks/Task10/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
                 new FileOutputStream("output.txt");
 
         for (int i = 0; i < figures.length; i++) {
-            String s[] = figures[i].toString();
+            String s = figures[i].toString();
             outputStream.write(s);
         }
     }

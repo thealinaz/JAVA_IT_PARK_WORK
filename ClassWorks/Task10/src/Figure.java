c08276cc38cacffbdf9ef0082466a659256f1120
Student7 <thealinaz@mail.ru>
2017-04-15 17:31:56 +0300

    add

---
+++ ClassWorks/Task10/src/Figure.java
@@ -0,0 +1,2 @@
+public class Figure implements Shape{
+}
---
+++ ClassWorks/Task10/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        Figure figures[] = new Figure[5];
+        figures[0] = new Triangle(3, 5, 4);
+        figures[1] = new Triangle(2, 3, 4);
+        figures[2] = new Rectangle(5, 10);
+        figures[3] = new Rectangle(2, 9);
+        figures[4] = new Square(8);
+
+        OutputStream outputStream =
+                new FileOutputStream("output.txt");
+
+        for (int i = 0; i < figures.length; i++) {
+            String s[] = figures[i].toString();
+            outputStream.write(s);
+        }
+    }
+}
